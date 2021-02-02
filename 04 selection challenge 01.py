4720eee3be4ef429a083b51f3abf1352ab199b46
AndreasMalekosOPHS <71763952+AndreasMalekosOPHS@users.noreply.github.com>
2021-02-02 11:31:43 +0000

    Delete 04 selection challenge 01.py

--- 04 selection challenge 01.py
+++
@@ -1,9 +0,0 @@
-number1 = int(input("Please enter a number here >>> "))
-
-number2 = int(input("Please enter a different number here >>> "))
-
-if number1 > number2:
-    print(number1, number2)
-
-else:
-    print(number2, number1)
